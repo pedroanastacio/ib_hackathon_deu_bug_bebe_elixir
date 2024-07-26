8801c5a518449867c1003f0704effd7f511cbfa2
Pedro Pereira <pedro.2108@hotmail.com>
2024-07-26 15:16:24 -0300

    feat: routes structure implemented

--- lib/router.ex
+++ lib/router.ex
@@ -4,7 +4,14 @@ defmodule Router do
   plug :match
   plug :dispatch
 
+  forward "/users", to: UsersRouter
+  forward "/transactions", to: TransactionsRouter
+
   get "/" do
     send_resp(conn, 200, "Deu bug, bebe elixir!!!")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Not found")
   end
 end

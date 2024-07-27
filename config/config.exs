37c925c87bfca1e9aaee7e933ac92c32c0d12633
vitor <pereiravitor1218@gmail.com>
2024-07-27 17:03:00 -0300

    chore: fixes hostname and add private_key in config

--- config/config.exs
+++ config/config.exs
@@ -5,8 +5,9 @@ config :deu_bug_bebe_elixir_ng, App.Repo,
   password: "codefest",
   database: "codefest",
   # Quando rodar localmente, use "localhost"
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  private_key: "B1BAC52A0652C1938018AB38EE0820940E9C19A9EA5FFC9D53BD87F6C145A089"
 
 config :deu_bug_bebe_elixir_ng, ecto_repos: [App.Repo]
